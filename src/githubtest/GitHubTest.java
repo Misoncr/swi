218f21a48bdadd0987667f222faca3f49005fa73
Misoncr <misoncr@gmail.com>
2015-12-09 11:49:32 +0100

    zmena O

--- src/githubtest/GitHubTest.java
+++ src/githubtest/GitHubTest.java
@@ -16,7 +16,7 @@ public class GitHubTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-	Syste.out.println("lol");
+	Syste.Out.println("lol");
     }
     
 }
